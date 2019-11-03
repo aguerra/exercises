5e2a5331685aa1c1832a131a93116dd7ff17b76a
Alex Guerra <alex9627@gmail.com>
2019-11-02 21:18:02 -0300

    Add dp fibonacci

--- src/dynamic_programming/fibonacci.clj
+++ src/dynamic_programming/fibonacci.clj
@@ -1,10 +1,23 @@
 (ns dynamic-programming.fibonacci)
 
-(def memoize-fib
+(defn fibonacci-tests
+  "Recurrence relation: f(i) = f(i-1) + f(i-2)."
+  [test-fn]
+  (assert (= 0 (test-fn 0)))
+  (assert (= 1 (test-fn 1)))
+  (assert (= 1 (test-fn 2)))
+  (assert (= 5 (test-fn 5)))
+  (assert (= 13 (test-fn 7)))
+  (assert (= 34 (test-fn 9))))
+
+(def memoized-fib
   (memoize (fn [n]
              (if (< n 2) n
-                 (+ (memoize-fib (dec n)) (memoize-fib (- n 2)))))))
+                 (+ (memoized-fib (dec n)) (memoized-fib (- n 2)))))))
 
-(defn bottom-up-fib [n]
+(defn bottom-up-fib
+  [n]
   (-> (map first (iterate (fn [[a b]] [b (+' a b)]) [0 1]))
       (nth n)))
+
+(run! fibonacci-tests [memoized-fib bottom-up-fib])
