f5e71bed9921cc829a12c609b1d4f1ce9fd38057
Alex Guerra <alex9627@gmail.com>
2020-01-12 20:11:01 -0300

    Add non-recursive powerset exercise
    Also improve tests and style.

--- src/recursion/powerset.clj
+++ src/recursion/powerset.clj
@@ -3,17 +3,23 @@
 
 (defn powerset
   "Write a function that, given a set, generates its power set."
-  [s]
-  (loop [[x & r] s
-         p '(())]
+  [coll]
+  (loop [[x & r] (seq coll)
+         p [[]]]
     (if x
       (recur r (concat p (map (partial cons x) p)))
       p)))
 
-(deftest powerset-tests
-  (is (= [[]] (powerset [])))
-  (is (= [[] [1]] (powerset '(1))))
-  (is (= [[] [1] [2] [2 1]] (powerset #{1 2})))
-  (is (= [[] [1] [2] [2 1] [3] [3 1] [3 2] [3 2 1]] (powerset [1 2 3]))))
+(defn powerset-reduce
+  [coll]
+  (reduce (fn [r x] (concat r (map (partial cons x) r))) [[]] (seq coll)))
+
+(deftest tests
+  (are [r coll] (= r (powerset coll) (powerset-reduce coll))
+    [[]]                                       []
+    [[] [1]]                                   '(1)
+    '(() (1) (2) (2 1))                        #{1 2}
+    [[] [[:a 1]]]                              {:a 1}
+    [[] [1] [2] [2 1] [3] [3 1] [3 2] [3 2 1]] [1 2 3]))
 
 (run-tests)
