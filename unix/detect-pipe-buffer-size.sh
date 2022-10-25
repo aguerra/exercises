1014484103b2f263628b6dd1c4d637ded8e8f08a
Alex Guerra <alex9627@gmail.com>
2022-10-25 19:52:40 -0300

    Update comments in detect-pipe-buffer-size.sh

--- unix/detect-pipe-buffer-size.sh
+++ unix/detect-pipe-buffer-size.sh
@@ -3,7 +3,6 @@
 # Write code to detect the default pipe buffer size.
 #
 # The shell starts the two programs in parallel but dd is really fast
-# so when we send the signal it's blocked because the pipe buffer is
-# full (you can use sleep to delay sending the signal).
+# so when we send the signal it's blocked because the pipe buffer is full.
 
 dd if=/dev/zero bs=1k | killall -s SIGINT dd
