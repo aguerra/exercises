45376e53d8ab60371c7705b22f4a55ef89efc026
Alex Guerra <alex9627@gmail.com>
2022-12-13 22:39:45 -0300

    Add simple quickselect implementation

---
+++ selection/quickselect.py
@@ -0,0 +1,47 @@
+"""Quickselect implementations."""
+
+
+def recursive(iterable, k):
+    """Return the kth smallest element.
+
+    >>> recursive([], 1) is None
+    True
+
+    >>> recursive([1, 2, 3, 4], 2)
+    2
+
+    >>> recursive([4, 3, 2, 1], 3)
+    3
+
+    >>> recursive([7, 8, 1, 10, 2], 4)
+    8
+
+    >>> recursive([1, 1, 1, 1, 2], 2)
+    2
+
+    >>> recursive([1, 1, 1, 1, 2], 3) is None
+    True
+    """
+    if not iterable:
+        return None
+    pivot = iterable[0]
+    less, greater = [], []
+
+    for item in iterable:
+        if item < pivot:
+            less.append(item)
+        elif item > pivot:
+            greater.append(item)
+
+    tmp = len(less) + 1
+    if tmp == k:
+        return pivot
+    elif tmp < k:
+        return recursive(greater, k - tmp)
+    else:
+        return recursive(less, k)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
